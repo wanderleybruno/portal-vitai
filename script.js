6440cab4144b2b1bdd525a35b298ebe2c7ad6196
Bruno Santos <120611328+wanderleybruno@users.noreply.github.com>
2023-02-14 00:17:17 -0300

    inserindo msg alert com js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-window.alert("Site em construção, dúvidas entrar em contato com:" + "\n" + "Bruno 21 965969650")
+window.alert("Site em construção, dúvidas entrar em contato com:" + "\n" + "Bruno 21 965969650 ou 21 980488310")
 
 
 
   window.onload = function() {
     var message = document.createElement("div");
-    message.innerHTML = "Este site está em construção. Em breve estará disponível para acesso.";
+    message.innerHTML = ("Este site está em construção.<br>Em breve estará disponível para acesso.<br> Dúvidas falar com: <br> Bruno 21 965969650 (Apenas WhatsApp)  <br> 21 980488310" );
     message.style.backgroundColor = "red";
     message.style.color = "white";
-    message.style.fontSize = "1.5em";
-    message.style.padding = "20px";
+    message.style.fontSize = "1.2em";
+    message.style.fontWeight = "bolder";
+    message.style.padding = "10px";
     message.style.textAlign = "center";
+   
+
     document.body.insertBefore(message, document.body.firstChild);
   };
